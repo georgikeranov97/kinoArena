b106a25ac265c7f6fbaa9fb7d2dc64b8434b3cb1
unknown <mirena.dim@gmail.com>
2019-02-03 11:29:33 +0200

    prenaredeni papki

--- kinoArena/scripts/flyingPopcorns.js
+++ kinoArena/scripts/flyingPopcorns.js
@@ -1,50 +1,94 @@
 var nacho1 = document.getElementById('nacho1')
+var nacho2 = document.getElementById('nacho2')
 var popcorn1 = document.getElementById('popcorn1')
 var popcorn2 = document.getElementById('popcorn2')
 var popcorn3 = document.getElementById('popcorn3')
 var popcorn4 = document.getElementById('popcorn4')
+var popcorn5 = document.getElementById('popcorn5')
+var popcorn6 = document.getElementById('popcorn6')
+var flyingStars = document.getElementById('starSky')
 
-nacho1.style.top= 208+'px';
-popcorn1.style.top = 81 + 'px';
-popcorn2.style.top = 300 + 'px';
-popcorn3.style.top = 392 +'px';
-popcorn4.style.top = 464 + 'px';
+nacho1.style.top= 767+'px';
+nacho2.style.top= 887+'px';
+popcorn1.style.top = 833 + 'px';
+popcorn2.style.top = 1000 + 'px';
+popcorn3.style.top = 1000 +'px';
+popcorn4.style.top = 691 + 'px';
+popcorn5.style.top = 1059 + 'px';
+popcorn6.style.top = 1128 + 'px';
+flyingStars.style.top = 300 + 'px'
 
-var flyingElements = [nacho1, popcorn1, popcorn2, popcorn3, popcorn4]
+var flyingElements = [nacho1, nacho2, popcorn1, popcorn2, popcorn3, popcorn4, popcorn5, popcorn6]
+
 //window.pageYOffset dava chislo pri scroll ot 0 do kraq na scroll-a
 //zapazvame si v lastNum, koe e bilo chisloto i sravnqvame s novoto num
 //ako staroto chislo, e po-golqmo, znachi se scroll-va nagore
 var lastNum=0;
-var flag = false;
+
 var id;
-var startmop;
+var id2;
+var imaLi=false;
+var count=0;
 window.addEventListener('scroll',()=>{
+    if(imaLi){return}
+    imaLi=true;
     let num=window.pageYOffset
     if (num>lastNum ){
-        flyingElements.forEach(e=> {
+        id=setInterval(moveDown,30)
+        id2=setInterval(moveSkyUp,30)
 
-            moveDown(e)
-        })
-           }else{
-        flyingElements.forEach(e=> {
-            moveUp(e)
-        }
-        )}
+    }else{
+       id=setInterval(moveUp,30)
+       id2=setInterval(moveSkyDown,30)
+    }
     lastNum=num;
 })
-function moveDown(obj){
-    var mop=parseInt(obj.style.top,10)
-        mop++;
-        obj.style.top= mop+ 'px'
-        obj.style.transform = 'rotate('+mop+'deg)';
+function moveDown(){
+    
+    flyingElements.forEach(function (el){
+        var mop=parseInt(el.style.top,10) 
+       mop++
+        
+        el.style.top= mop+ 'px'
+        el.style.transform = 'rotate('+mop+'deg)';
+    })
+    count++;
+    if(count>=40){
+        count=0;
+        imaLi=false;
+        clearInterval(id)
     }
-
+}
    
-function moveUp(obj){
-    var mop=parseInt(obj.style.top,10)
-        mop--;
-        obj.style.top= mop+ 'px'
-        obj.style.transform = 'rotate('+mop+'deg)';
-  
+function moveUp(){
 
+    flyingElements.forEach(el =>{
+        var mop=parseInt(el.style.top,10)
+            mop--;
+    
+        el.style.top= mop+ 'px'
+        el.style.transform = 'rotate('+mop+'deg)';
+    })
+    count++;
+    if(count>=40){
+        count=0;
+        imaLi=false;
+        clearInterval(id)
+    }
+}
+function moveSkyUp(){
+    var mop=parseInt(flyingStars.style.top,10)
+    mop--;
+    flyingStars.style.top= mop +'px'
+    if(count>=39){
+        clearInterval(id2)
+}
+}
+function moveSkyDown(){
+    var mop=parseInt(flyingStars.style.top,10)
+    mop++;
+    flyingStars.style.top= mop +'px'
+    if(count>=39){
+        clearInterval(id2)
+}
 }
